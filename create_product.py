9cf3820da8ff812cbfc2245977bdb436185690ed
Malte Ferber <malte.ferber@fn.de>
2024-10-13 12:51:59 +0200

    added lines in create_product, so first steps are automised (only when for the custom panes). Next steps are: - what are the different qualities in a gpd file between a lake and a Rodungstrasse, so a difference can be detected.

--- create_product.py
+++ create_product.py
@@ -16,8 +16,141 @@ from pathlib import Path
 # Define the folder path
 folder_path = Path('./clean_data')
 
+
+# Create a base map - to be automized with the location and zoom!
+m = folium.Map(location=[51.0, 6.5], zoom_start=10, min_zoom=8)
+
+# Set up custome map panes for points, lines, polygons
+folium.map.CustomPane("pointsPane", z_index=330).add_to(m)
+folium.map.CustomPane("linesPane", z_index=320).add_to(m)
+folium.map.CustomPane("polygonsPane", z_index=310).add_to(m)
+
+
+# add Esri World Imagery tile layer,
+# (see [Leaflet Provider Demo](https://leaflet-extras.github.io/leaflet-providers/preview/) for a quick preview of various basemaps).
+folium.TileLayer(
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Esri',
+    name='Esri.WorldImagery',
+    overlay=False,
+    control=True,
+    min_zoom=8,
+    show=False  # hide layer when opening the map
+).add_to(m)
+# add grey canvas tile layer
+folium.TileLayer(
+    tiles='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    attr='Carto',
+    name='Grey Canvas',
+    overlay=False,
+    control=True,
+    min_zoom=8,
+    show=True  # hide layer when opening the map
+).add_to(m)
+
+
 # loop over every file in folder "clean_data"
 for file_path in folder_path.iterdir():
     if file_path.is_file():  # Check if it is a file (not a directory)
         print(f"Processing file: {file_path.name}")
-        # Add your file processing code here
+        print(f"{file_path}")
+        current = gpd.read_file(f"./{file_path}", layer=file_path.name)
+
+        # adopt this according to dictionary!
+        # what are the qualities of a linesPane and a polygonsPane?
+        # layer "type" must be "Polygon" or "Multipolygon" if layer name = etc. ->
+
+        if current.geom_type in [Polygon, MultiPolygon]:  # for lakes, buildings etc
+            pane = "polygonsPane"
+        elif current.geom_type == Point:
+            pane = "pointsPane"
+        else:
+            # elif current.geom_type in [LineString, MultiLineString]:
+            pane = "linesPane"
+
+
+        # for lakes:
+        folium.GeoJson(
+            current,
+            name=file_path.name,
+            pane=pane,
+            style_function=lambda feature: {
+                'color': '#4e4eff',
+                'fillColor': "#4e4eff",
+                'fillOpacity': 0.8,
+                'fill': True,
+                'weight': 0
+                },
+            tooltip=file_path.name
+            ).add_to(m)
+
+
+# save the map as hmtl
+m.save('pipe_map.html')
+
+# # --- Just to compare --- #
+# # See
+# folium.GeoJson(
+#     hambacher_see,
+#     name="Hambacher See",
+#     pane="polygonsPane",
+#     style_function=lambda feature: {
+#         'color': '#4e4eff',
+#         'fillColor': "#4e4eff",
+#         'fillOpacity': 0.8,
+#         'fill': True,
+#         'weight': 0
+#     },
+#     tooltip="Hambacher See"
+# ).add_to(m)
+
+# # Pipeline
+# folium.GeoJson(
+#     pipelines_new,
+#     name="Wasserleitungen",  # again, determine the name for the layer toggle
+#     pane="linesPane",
+#     style_function=lambda feature: {
+#         'color': '#6b6bc9',  # nif feature['properties']['Name'] == 'Bündelungsleitung' else '#3f3f3f',
+#         'weight': 5  # feature['properties']["Leistung (m3/s)"]
+#     },
+#     tooltip=folium.GeoJsonTooltip(fields=['Name'], labels=False),
+#     popup=folium.GeoJsonPopup(fields=['Name', "Strecke", "Länge (km)", "Leistung (m3/s)", "Trassenbreite (m)", "Anzahl der Röhren"])
+# ).add_to(m)
+
+# # Clearing / Rodungstrasse
+# folium.GeoJson(
+#     data=clearing,
+#     name="Rodungstrasse",  # again, determine the name for the layer toggle
+#     pane="polygonsPane",
+#     style_function=lambda feature: {
+#         'color': 'brown',
+#         'weight': 1,
+#         'fillColor': 'brown',
+#         'fillOpacity': 0.3
+#     }, tooltip="Rodungstrasse"
+# ).add_to(m)
+
+# # points of interests like constructions, and destructions
+# folium.GeoJson(
+#     destructions,
+#     name="Zerstörungen",  # again, determine the name for the layer toggle
+#     pane="pointsPane",
+#     marker=folium.Marker(
+#         icon=folium.Icon(color='lightgreen', icon='burst', prefix='fa')
+#     ),
+#     tooltip=folium.GeoJsonTooltip(fields=['Name'], labels=False)
+# ).add_to(m)
+
+# # Verteilwerk / Pumpbauwerk
+# folium.GeoJson(
+#     pumpwerk,
+#     name="Pumpbauwerk",  # again, determine the name for the layer toggle
+#     pane="linesPane",
+#     style_function=lambda feature: {
+#         'color': 'black',
+#         'weight': 1,
+#         'fillColor': 'black',
+#         'fillOpacity': 1
+#     },
+#     tooltip=folium.GeoJsonTooltip(fields=['name'], labels=False)
+# ).add_to(m)
--- pipe_map.py
+++ pipe_map.py
@@ -18,15 +18,15 @@ import json
 # ---- Visualization ----
 
 # Create a base map
-m = folium.Map(location = [51.0, 6.5], zoom_start = 10,  min_zoom = 8)
+m = folium.Map(location=[51.0, 6.5], zoom_start=10, min_zoom=8)
 
 # Set up custome map panes for points, lines, polygons
-folium.map.CustomPane("pointsPane", z_index = 330).add_to(m)
-folium.map.CustomPane("linesPane", z_index = 320).add_to(m)
-folium.map.CustomPane("polygonsPane", z_index = 310).add_to(m)
+folium.map.CustomPane("pointsPane", z_index=330).add_to(m)
+folium.map.CustomPane("linesPane", z_index=320).add_to(m)
+folium.map.CustomPane("polygonsPane", z_index=310).add_to(m)
 
 
-# Add `Esri.WorldImagery` tile layer 
+# Add `Esri.WorldImagery` tile layer
 # (see [Leaflet Provider Demo](https://leaflet-extras.github.io/leaflet-providers/preview/) for a quick preview of various basemaps).
 
 # add Esri World Imagery tile layer
@@ -36,8 +36,8 @@ folium.TileLayer(
     name='Esri.WorldImagery',
     overlay=False,
     control=True,
-    min_zoom = 8,
-    show=False # hide layer when opening the map
+    min_zoom=8,
+    show=False  # hide layer when opening the map
 ).add_to(m)
 
 # add grey canvas tile layer
@@ -47,82 +47,81 @@ folium.TileLayer(
     name='Grey Canvas',
     overlay=False,
     control=True,
-    min_zoom = 8,
-    show=True # hide layer when opening the map
+    min_zoom=8,
+    show=True  # hide layer when opening the map
 ).add_to(m)
 
 
-hambacher_see = gpd.read_file("../data/hambachersee.gpkg", layer = 'fid_2')
-#hambacher_see = hambacher_see.set_geometry([Polygon(hambacher_see.get_coordinates())])
-hambacher_see_manheim = gpd.read_file("../data/hambachersee_manheim.gpkg", layer = 'fid_1')
+hambacher_see = gpd.read_file("../data/hambachersee.gpkg", layer='fid_2')
+# hambacher_see = hambacher_see.set_geometry([Polygon(hambacher_see.get_coordinates())])
+hambacher_see_manheim = gpd.read_file("../data/hambachersee_manheim.gpkg", layer='fid_1')
 # hambacher_see_kerpen = hambacher_see_kerpen.set_geometry([Polygon(hambacher_see_kerpen.get_coordinates())])
 hambacher_see_manheim
-#hambacher_see.geometry = hambacher_see.geometry.apply(lambda x: Polygon(x.get_coordinates()))
+# hambacher_see.geometry = hambacher_see.geometry.apply(lambda x: Polygon(x.get_coordinates()))
 
 
-garzweiler_see = gpd.read_file("../data/garzweilersee_wasser.gpkg", layer = 'garzweilersee_wasser')
-#garzweiler_see = garzweiler_see.set_geometry([Polygon(garzweiler_see.get_coordinates())])
-inde_see = gpd.read_file("../data/indersee_gp.gpkg", layer = 'inder_see_wasser')
+garzweiler_see = gpd.read_file("../data/garzweilersee_wasser.gpkg", layer='garzweilersee_wasser')
+# garzweiler_see = garzweiler_see.set_geometry([Polygon(garzweiler_see.get_coordinates())])
+inde_see = gpd.read_file("../data/indersee_gp.gpkg", layer='inder_see_wasser')
 inde_see = inde_see.set_geometry([Polygon(inde_see.get_coordinates())])
 
 garzweiler_see
 
 
-
 folium.GeoJson(
-    hambacher_see, 
-    name="Hambacher See", 
-    pane = "polygonsPane",
-    style_function = lambda feature: {
+    hambacher_see,
+    name="Hambacher See",
+    pane="polygonsPane",
+    style_function=lambda feature: {
         'color': '#4e4eff',
         'fillColor': "#4e4eff",
         'fillOpacity': 0.8,
         'fill': True,
         'weight': 0
     },
-    tooltip = "Hambacher See"
+    tooltip="Hambacher See"
 ).add_to(m)
 
 folium.GeoJson(
-    hambacher_see_manheim, 
-    name="Hambacher See (Manheim)", 
-    pane = "polygonsPane",
-    style_function = lambda feature: {
+    hambacher_see_manheim,
+    name="Hambacher See (Manheim)",
+    pane="polygonsPane",
+    style_function=lambda feature: {
         'color': '#9c9cff',
         'fillColor': "#9c9cff",
         'fillOpacity': 0.8,
         'fill': True,
         'weight': 0
     },
-    tooltip = "Hambacher See (Manheim) - geplant"
+    tooltip="Hambacher See (Manheim) - geplant"
 ).add_to(m)
 
 folium.GeoJson(
-    garzweiler_see, 
-    name="Garzweiler See", 
-    pane = "polygonsPane",
-    style_function = lambda feature: {
+    garzweiler_see,
+    name="Garzweiler See",
+    pane="polygonsPane",
+    style_function=lambda feature: {
         'color': '#4e4eff',
         'fillColor': "#4e4eff",
         'fillOpacity': 0.8,
         'fill': True,
         'weight': 0
     },
-    tooltip = "Garzweiler See"
+    tooltip="Garzweiler See"
 ).add_to(m)
 
 folium.GeoJson(
-    inde_see, 
-    name="Inde See", 
-    pane = "polygonsPane",
-    style_function = lambda feature: {
+    inde_see,
+    name="Inde See",
+    pane="polygonsPane",
+    style_function=lambda feature: {
         'color': '#4e4eff',
         'fillColor': "#4e4eff",
         'fillOpacity': 0.8,
         'fill': True,
         'weight': 0
     },
-    tooltip = "Inde See"
+    tooltip="Inde See"
 ).add_to(m)
 
 
@@ -134,8 +133,7 @@ pipelines.Name = ["Transportleitung West", "Transportleitung Süd"]
 # pipelines
 
 
-
-pipelines = pipelines.to_crs("EPSG:4326") # change CRS to the same as in 'consumer'
+pipelines = pipelines.to_crs("EPSG:4326")  # change CRS to the same as in 'consumer'
 pipelines.crs
 
 pt = pipelines[pipelines.Name == "Transportleitung Süd"].get_coordinates().iloc[0]
@@ -143,13 +141,13 @@ pt = Point(pt)
 
 print(pt.coords)
 
-#snap(pt, pipelines.geometry.union_all(), tolerance=1)
+# snap(pt, pipelines.geometry.union_all(), tolerance=1)
 
 jnk = split(pipelines.geometry.union_all(), snap(pt, pipelines.geometry.union_all(), tolerance=1))
 jnk
-jnk = gpd.GeoDataFrame(geometry = [jnk]).explode(column="geometry", )
+jnk = gpd.GeoDataFrame(geometry=[jnk]).explode(column="geometry", )
 
-pipelines_new = jnk.assign(Name = ["Bündelungsleitung", "Garzweilerleitung", "Hambachleitung"])
+pipelines_new = jnk.assign(Name=["Bündelungsleitung", "Garzweilerleitung", "Hambachleitung"])
 
 
 pipelines_new = gpd.GeoDataFrame(pipelines_new).set_crs("EPSG:4326")
@@ -157,24 +155,22 @@ pipelines_new = gpd.GeoDataFrame(pipelines_new).set_crs("EPSG:4326")
 daten_pipelines = pd.read_csv("../data/Daten_Pipelines.csv")
 daten_pipelines
 
-pipelines_new = pd.merge(pipelines_new, daten_pipelines, on = "Name")
+pipelines_new = pd.merge(pipelines_new, daten_pipelines, on="Name")
 pipelines_new.columns
 
 
 # Add the GeoDataFrame to the map
 folium.GeoJson(
-    pipelines_new, 
-    name="Wasserleitungen", # again, determine the name for the layer toggle
-    pane = "linesPane",
-    style_function = lambda feature: {
-        'color': '#6b6bc9', # nif feature['properties']['Name'] == 'Bündelungsleitung' else '#3f3f3f',
-        'weight': 5 # feature['properties']["Leistung (m3/s)"]
+    pipelines_new,
+    name="Wasserleitungen",  # again, determine the name for the layer toggle
+    pane="linesPane",
+    style_function=lambda feature: {
+        'color': '#6b6bc9',  # nif feature['properties']['Name'] == 'Bündelungsleitung' else '#3f3f3f',
+        'weight': 5  # feature['properties']["Leistung (m3/s)"]
     },
-    tooltip = folium.GeoJsonTooltip(fields=['Name'], labels = False),
-    popup = folium.GeoJsonPopup(fields=['Name', "Strecke", "Länge (km)", "Leistung (m3/s)", "Trassenbreite (m)", "Anzahl der Röhren"])
+    tooltip=folium.GeoJsonTooltip(fields=['Name'], labels=False),
+    popup=folium.GeoJsonPopup(fields=['Name', "Strecke", "Länge (km)", "Leistung (m3/s)", "Trassenbreite (m)", "Anzahl der Röhren"])
 ).add_to(m)
-
-
 
 
 # load GeoPackage file for clearing
@@ -184,18 +180,17 @@ clearing = gpd.read_file("../data/rodungstrasse.gpkg")
 clearing
 
 
-
 # Add the GeoDataFrame to the map
 folium.GeoJson(
-    data = clearing, 
-    name = "Rodungstrasse", # again, determine the name for the layer toggle
-    pane = "polygonsPane",
-    style_function = lambda feature: {
+    data=clearing,
+    name="Rodungstrasse",  # again, determine the name for the layer toggle
+    pane="polygonsPane",
+    style_function=lambda feature: {
         'color': 'brown',
         'weight': 1,
         'fillColor': 'brown',
-        'fillOpacity': 0.3         
-    },tooltip = "Rodungstrasse"
+        'fillOpacity': 0.3
+    }, tooltip="Rodungstrasse"
 ).add_to(m)
 
 
@@ -203,7 +198,7 @@ folium.GeoJson(
 destruction_data = gpd.read_file("../data/Rheinwassertransportleitung.kml")
 
 # polys = destructions[destructions.geom_type=='Polygon']
-points = destruction_data[destruction_data.geom_type=='Point']
+points = destruction_data[destruction_data.geom_type == 'Point']
 
 destructions_idx = points['Name'].str.contains('Zerstörung')
 destructions = points[destructions_idx]
@@ -217,28 +212,26 @@ constructions
 # points
 
 
-
 folium.GeoJson(
-    destructions, 
-    name = "Zerstörungen", # again, determine the name for the layer toggle
-    pane = "pointsPane",
+    destructions,
+    name="Zerstörungen",  # again, determine the name for the layer toggle
+    pane="pointsPane",
     marker=folium.Marker(
         icon=folium.Icon(color='lightgreen', icon='burst', prefix='fa')
     ),
-    tooltip = folium.GeoJsonTooltip(fields=['Name'], labels = False)
+    tooltip=folium.GeoJsonTooltip(fields=['Name'], labels=False)
 ).add_to(m)
 
 
 folium.GeoJson(
-    constructions, 
-    name = "Baumaßnahmen", # again, determine the name for the layer toggle
-    pane = "pointsPane",
+    constructions,
+    name="Baumaßnahmen",  # again, determine the name for the layer toggle
+    pane="pointsPane",
     marker=folium.Marker(
         icon=folium.Icon(color='lightred', icon='person-digging', prefix='fa')
     ),
-    tooltip = folium.GeoJsonTooltip(fields=['Name'], labels = False)
+    tooltip=folium.GeoJsonTooltip(fields=['Name'], labels=False)
 ).add_to(m)
-
 
 
 pumpwerk = gpd.read_file("../data/Pumpbauwerk_Ufer.gpkg")
@@ -249,44 +242,38 @@ verteilwerk = gpd.read_file("../data/Verteilbauwerk.gpkg")
 verteilwerk
 
 
-
 folium.GeoJson(
-    pumpwerk, 
-    name = "Pumpbauwerk", # again, determine the name for the layer toggle
-    pane = "linesPane",
-    style_function = lambda feature: {
-        'color': 'black',
-        'weight': 1,
-        'fillColor': 'black',
-        'fillOpacity': 1         
-    },
-    tooltip = folium.GeoJsonTooltip(fields=['name'], labels = False)
-).add_to(m)
-
-
-
-folium.GeoJson(
-    verteilwerk, 
-    name = "Verteilbauwerk", # again, determine the name for the layer toggle
-    pane = "linesPane",
-    style_function = lambda feature: {
+    pumpwerk,
+    name="Pumpbauwerk",  # again, determine the name for the layer toggle
+    pane="linesPane",
+    style_function=lambda feature: {
         'color': 'black',
         'weight': 1,
         'fillColor': 'black',
         'fillOpacity': 1
     },
-    tooltip = folium.GeoJsonTooltip(fields=['name'], labels = False)
+    tooltip=folium.GeoJsonTooltip(fields=['name'], labels=False)
 ).add_to(m)
 
 
+folium.GeoJson(
+    verteilwerk,
+    name="Verteilbauwerk",  # again, determine the name for the layer toggle
+    pane="linesPane",
+    style_function=lambda feature: {
+        'color': 'black',
+        'weight': 1,
+        'fillColor': 'black',
+        'fillOpacity': 1
+    },
+    tooltip=folium.GeoJsonTooltip(fields=['name'], labels=False)
+).add_to(m)
 
 
 # Add layer control to toggle layers
 folium.LayerControl().add_to(m)
-#m
+# m
 m.show_in_browser()
-
-
 
 
 # save the map as hmtl
